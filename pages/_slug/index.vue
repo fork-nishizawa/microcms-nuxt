12199d893975594941b36500e8d17faf19c541bc
yoh.nishizawa <nishizawa@fork.co.jp>
2021-03-23 16:41:41 +0900

    ✨ 詳細ページ作成
    #1

---
+++ pages/_slug/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-row(justify="center" align="center")
+    v-col(cols="12" sm="8" md="6")
+      v-title
+        | {{ title }}
+      .post(v-html="body")
+</template>
+
+<script lang="ts">
+import { Context } from '@nuxt/types'
+import axios from 'axios'
+
+export default {
+  async asyncData(context: Context) {
+    const { data } = await axios.get(
+      `https://fork-yoh.microcms.io/api/v1/blog/${context.params.slug}`,
+      {
+        headers: { 'X-API-KEY': context.$config.apiKey }
+      }
+    )
+    return data
+  }
+}
+</script>
