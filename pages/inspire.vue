a1f965e8c383c2a537f9cd0bfa01011c8e121382
yoh.nishizawa <nishizawa@fork.co.jp>
2021-03-23 15:16:13 +0900

    ✨ pug化
    #1

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,11 @@
-<template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+<template lang="pug">
+  v-row
+    v-col.text-center
+      img.mb-5(src="/v.png" alt="Vuetify.js")
+      blockquote.blockquote
+        | &#8220;First, solve the problem. Then, write the code.&#8221;
+        footer
+          small
+            em
+              |&mdash;John Johnson
 </template>
